205524ab51328cfa90522c27a56d2484d7e3def6
Ulrich HINVI <hinviulrich@gmail.com>
2022-10-26 03:12:49 +0200

    fixed

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		String name = "Rico";
+
+		System.out.println(name);
+		// "not good"
+
 	}
 
 }
